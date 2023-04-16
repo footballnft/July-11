84c4d981bd5e4f24e1fe6346d10731ea7fbd7cfe
footballnft <85647902+footballnft@users.noreply.github.com>
2023-04-16 19:08:46 +0100

    cors-3

--- apis/proxy-worker/src/index.ts
+++ apis/proxy-worker/src/index.ts
@@ -9,7 +9,7 @@ const router = Router()
 
 router.post('/bsc-exchange', async (request, _, headers: Headers) => {
   const ip = headers.get('X-Forwarded-For') || headers.get('Cf-Connecting-Ip') || ''
-  const isLocalHost = headers.get('origin') === 'https://july-11.vercel.app'
+  const isLocalHost = headers.get('origin') === '*'
   const body = (await request.text?.()) as any
 
   if (!body) return error(400, 'Missing body')
